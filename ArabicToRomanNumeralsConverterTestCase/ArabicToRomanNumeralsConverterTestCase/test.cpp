ec6957e15befbc761d31c62d660c36d3818f590b
仲 仲 <2544054211@qq.com>
2020-07-07 18:07:41 +0800

    编写了一个简单的测试单元。

--- ArabicToRomanNumeralsConverterTestCase/ArabicToRomanNumeralsConverterTestCase/test.cpp
+++ ArabicToRomanNumeralsConverterTestCase/ArabicToRomanNumeralsConverterTestCase/test.cpp
@@ -1,6 +1,15 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+int main(int argc, char* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
+
+TEST(AragicToRomanNumeralsConverterTestCase, PreparationsCompleted)
+{
+	// GTEST_FAIL();
+	GTEST_SUCCEED();
+}
+
+
