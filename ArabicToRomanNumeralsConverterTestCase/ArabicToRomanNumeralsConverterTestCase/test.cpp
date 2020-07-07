acd260e15de884294c18f0a309d65fe85e58fae8
仲 仲 <2544054211@qq.com>
2020-07-07 18:10:57 +0800

    case1：成功将1转换为I

--- ArabicToRomanNumeralsConverterTestCase/ArabicToRomanNumeralsConverterTestCase/test.cpp
+++ ArabicToRomanNumeralsConverterTestCase/ArabicToRomanNumeralsConverterTestCase/test.cpp
@@ -12,4 +12,17 @@ TEST(AragicToRomanNumeralsConverterTestCase, PreparationsCompleted)
 	GTEST_SUCCEED();
 }
 
+/// <summary>
+/// 将给定正整数参数转换为罗马数字字符串
+/// </summary>
+/// <param name="arabicNumber"></param>
+/// <returns></returns>
+std::string convertArabicNumberToRomanNumeral(const unsigned int arabicNumber)
+{
+	return "I";
+}
 
+TEST(AragicToRomanNumeralsConverterTestCase, 1_isConvertedTo_I)
+{
+	ASSERT_EQ("I", convertArabicNumberToRomanNumeral(1));
+}
